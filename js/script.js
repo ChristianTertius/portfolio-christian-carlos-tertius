0e68398deec9f0e807dcc87c49c1e0d809839651
ChristianTertius <ct160103@gmail.com>
2023-09-26 20:32:14 +0700

    Projects added + TypeIt Effect + Contact Form Function + some Changes

--- js/script.js
+++ js/script.js
@@ -36,20 +36,41 @@ window.addEventListener("click", function (e) {
 const darkToggle = document.querySelector("#dark-toggle");
 const html = document.querySelector("html");
 
-darkToggle.addEventListener("click", function(){
-  if(darkToggle.checked){
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
     html.classList.add("dark");
     localStorage.theme = "dark";
-  } else{
+  } else {
     html.classList.remove("dark");
     localStorage.theme = "light";
   }
-})
+});
 
 // pindahkan posisi toggle sesuai mode
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
   darkToggle.checked = true;
 } else {
   darkToggle.checked = false;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // TypeWriter Text Effect
+  new TypeIt("#el", {
+    startDelete: true,
+    loop: true,
+    waitUntilVisible: true,
+  })
+
+    .type("Programming.")
+    .pause(1000)
+    .empty()
+    .type("Hearing Music.")
+    .pause(1000)
+    .empty()
+    .type("Watching Dramas.")
+    .pause(1000)
+    .empty()
+    .go();
+
+});
